94f227189a6d22d8e46a6cb0f2349d42745781a5
Pierrick Taïly <pierrick.tly@gmail.com>
2023-10-30 18:36:07 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,6 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-      FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-      FIREBASE_MESSAGIN_SENDER_ID: process.env.FIREBASE_MESSAGIN_SENDER_ID,
-      FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-      FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
       STRAPI_URL: process.env.STRAPI_URL,
     },
   },
@@ -44,19 +37,6 @@ export default defineNuxtConfig({
       },
     },
   },
-
-  // vuefire: {
-  //   auth: true,
-  //   config: {
-  //     apiKey: process.env.FIREBASE_API_KEY,
-  //     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  //     projectId: process.env.FIREBASE_PROJECT_ID,
-  //     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  //     messagingSenderId: process.env.FIREBASE_MESSAGIN_SENDER_ID,
-  //     appId: process.env.FIREBASE_APP_ID,
-  //     measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-  //   },
-  // },
 
   pinia: {
     autoImports: ['defineStore'],
