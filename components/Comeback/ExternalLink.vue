ecd0375ebd41e6f8392e919797e1e2de0d768a4e
Pierrick Taïly <pierrick.tly@gmail.com>
2023-10-31 07:43:21 +0100

    Update ExternalLink.vue

--- components/Comeback/ExternalLink.vue
+++ components/Comeback/ExternalLink.vue
@@ -25,69 +25,33 @@ const { link, name } = defineProps({
 
 const icon = computed(() => {
   if (link.includes('youtube') && link.includes('music.')) {
-    return '~/youtube_music.png'
+    return '@/youtube_music.png'
   } else if (link.includes('youtube')) {
-    return '~/youtube.png'
+    return '@/youtube.png'
   } else if (link.includes('apple') && link.includes('music')) {
-    return '~/apple_music.png'
+    return '@/apple_music.png'
   } else if (link.includes('spotify')) {
-    return '~/spotify.png'
+    return '@/spotify.png'
   } else if (link.includes('tidal')) {
-    return '~/tidal.png'
+    return '@/tidal.png'
   } else if (link.includes('soundcloud')) {
-    return '~/sound_cloud.png'
+    return '@/sound_cloud.png'
   } else if (link.includes('facebook')) {
-    return '~/facebook.png'
+    return '@/facebook.png'
   } else if (link.includes('instagram')) {
-    return '~/instagram.png'
+    return '@/instagram.png'
   } else if (link.includes('twitter') || link.includes('x.com')) {
-    return '~/x.png'
+    return '@/x.png'
   } else if (link.includes('tiktok')) {
-    return '~/tiktok.png'
+    return '@/tiktok.png'
   } else if (link.includes('snapchat')) {
-    return '~/snapchat.png'
+    return '@/snapchat.png'
   } else if (link.includes('weibo')) {
-    return '~/weibo.png'
+    return '@/weibo.png'
   } else if (link.includes('deezer')) {
-    return '~/deezer.png'
+    return '@/deezer.png'
   } else {
-    return '~/default.png'
+    return '@/default.png'
   }
 })
-
-// onMounted(() => {
-//   selectIcon()
-// })
-
-// const icon = ref('')
-
-// function selectIcon() {
-//   if (link.includes('youtube') && link.includes('music.')) {
-//     icon.value = '~/youtube_music.png'
-//   } else if (link.includes('youtube')) {
-//     icon.value = '~/youtube.png'
-//   } else if (link.includes('apple') && link.includes('music')) {
-//     icon.value = '~/apple_music.png'
-//   } else if (link.includes('spotify')) {
-//     icon.value = '~/spotify.png'
-//   } else if (link.includes('tidal')) {
-//     icon.value = '~/tidal.png'
-//   } else if (link.includes('soundcloud')) {
-//     icon.value = '~/sound_cloud.png'
-//   } else if (link.includes('facebook')) {
-//     icon.value = '~/facebook.png'
-//   } else if (link.includes('instagram')) {
-//     icon.value = '~/instagram.png'
-//   } else if (link.includes('twitter') || link.includes('x.com')) {
-//     icon.value = '~/x.png'
-//   } else if (link.includes('tiktok')) {
-//     icon.value = '~/tiktok.png'
-//   } else if (link.includes('snapchat')) {
-//     icon.value = '~/snapchat.png'
-//   } else if (link.includes('weibo')) {
-//     icon.value = '~/weibo.png'
-//   } else {
-//     icon.value = '~/default.png'
-//   }
-// }
 </script>
