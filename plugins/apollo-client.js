ded63ca2aeb9648a52145952ed7ae6ae7b1fa380
Pierrick Taïly <pierrick.tly@gmail.com>
2023-11-20 21:22:54 +0100

    Add authentication to Apollo client

--- plugins/apollo-client.js
+++ plugins/apollo-client.js
@@ -1,6 +1,7 @@
 // plugins/apollo-client.js
 
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client/core'
+import { setContext } from '@apollo/client/link/context'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig()
@@ -9,11 +10,22 @@ export default defineNuxtPlugin((nuxtApp) => {
     uri: config.public.STRAPI_URL, // Remplacez par votre URL GraphQL
   })
 
+  const authLink = setContext((_, { headers }) => {
+    const token = config.public.STRAPI_KEY
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    }
+  })
+
+  const link = authLink.concat(httpLink)
+
   const cache = new InMemoryCache()
 
   const apolloClient = new ApolloClient({
-    link: httpLink,
-    key: config.public.STRAPI_KEY,
+    link: link,
     cache,
   })
 
