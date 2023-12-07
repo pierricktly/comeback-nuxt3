8e185293427bb0d53d7f0f6fb05bf1551b921739
Pierrick Taïly <pierrick.tly@gmail.com>
2023-12-07 10:39:26 +0100

    add modal

--- components/navigation.vue
+++ components/navigation.vue
@@ -1,4 +1,8 @@
 <script setup>
+import * as Mdl from '@kouts/vue-modal'
+
+const { Modal } = Mdl
+const showModal = ref(false)
 const routeN = useRoute()
 
 const navbar = ref(null)
@@ -84,5 +88,18 @@ function handleScroll() {
         </nav>
       </div>
     </div>
+    <Modal
+      v-model="showModal"
+      title="Add a News"
+      wrapper-class="animate__animated modal-wrapper"
+      :modal-style="{ background: '#1F1D1D', 'border-radius': '0.25rem', color: 'white' }"
+      :in-class="`animate__fadeInDown`"
+      :out-class="`animate__bounceOut`"
+      bg-class="animate__animated"
+      :bg-in-class="`animate__fadeInUp`"
+      :bg-out-class="`animate__fadeOutDown`"
+    >
+      <NewsCreation :artistList="artistFetch" @close-modal="showModal = false"/>
+    </Modal>
   </div>
 </template>
